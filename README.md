45ff8d2d74c611a6767fab98a0f5a53e82476970
gog <boo.factor@gmail.com>
2019-11-01 16:04:07 +0100

    adding barebone code structure

--- README.md
+++
@@ -1,9 +0,0 @@
-# Basic Scala Project Skeleon
-
-### Bootstrapped with
-
-1. Ammonite
-
-2. Globals File
-
-3. Scala Mock Tests Lib
---
+++ project/versions.scala
@@ -0,0 +1,9 @@
+
+object Versions {
+
+  def app = "0.1"
+  def scala = "2.12.10"
+  def ammonite = "1.8.1"
+  def scalatest = "3.0.5"
+
+}
